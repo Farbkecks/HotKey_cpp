b2eeb4cd78a0263bbe79ce4b7f866f80910d5621
Farbkecks <98021172+Farbkecks@users.noreply.github.com>
2023-03-17 17:16:56 +0100

    get volume (schlecht)

--- main.cpp
+++ main.cpp
@@ -36,22 +36,23 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-    string oAuth_Tocken = "";
+    string oAuth_Tocken = "BQB7Ne8EoBFdZSa_xjqPIrbZFY_AodlOXKW_D_Jfx6k2Ktgik6tceRSN0Wdt0uT1LO4QmYQ7lCWv3DteX2SEOFnwXFq49jK-Yr2SULvTLJJSHbp2KWWvdiE2F3GvAXHsfVl2xQkCOTO3VINI69qYI38Ww0Gm9DOi5-Fti1eSLUzWm6fexuswVbp94w";
 
-    cpr::Response r = cpr::Put(cpr::Url{"https://api.spotify.com/v1/me/player/pause"},
+//    cpr::Response r = cpr::Put(cpr::Url{"https://api.spotify.com/v1/me/player/volume?volume_percent=%2B50"},
+//                               cpr::Header{{"Content-Length", "0"}},
+//                               cpr::Bearer{oAuth_Tocken});
+//
+//    cout << r.status_code << endl;
+
+//    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+//
+    cpr::Response r = cpr::Get(cpr::Url{"https://api.spotify.com/v1/me/player"},
                                cpr::Header{{"Content-Length", "0"}},
                                cpr::Bearer{oAuth_Tocken});
-
-    cout << r.status_code << endl;
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-    r = cpr::Put(cpr::Url{"https://api.spotify.com/v1/me/player/play"},
-                               cpr::Header{{"Content-Length", "0"}},
-                               cpr::Bearer{oAuth_Tocken});
-
-
-    cout << r.status_code << endl;
-
+    string resulte = r.text;
+    cout << resulte << endl;
+    auto pos = resulte.find("volume_percent");
+    auto volumeString = resulte.substr(pos+18,3);
+    int volume = std::stoi(volumeString);
     return 0;
 }
